535d7234822dee2ecfe654215b324e1ea6221920
Yossi Rizgan <yossi.rizgan@kik.com>
2018-03-06 09:53:29 +0200

    fail tests to check correct reporting in travis

--- kin-core/src/androidTest/java/kin/core/KinAccountIntegrationTest.java
+++ kin-core/src/androidTest/java/kin/core/KinAccountIntegrationTest.java
@@ -343,6 +343,7 @@ public class KinAccountIntegrationTest {
 
     @Test
     public void getPublicAddress_DeletedAccount_EmptyPublicAddress() throws Exception {
+        fail("test failure in travis!!");
         KinAccount kinAccount = kinClient.addAccount(PASSPHRASE);
         kinClient.deleteAccount(0, PASSPHRASE);
         assertNull(kinAccount.getPublicAddress());
