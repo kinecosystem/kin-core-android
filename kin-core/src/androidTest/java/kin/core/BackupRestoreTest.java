aa1bf791673c4c5e9a8a1377e38360e5d6b2a669
Yossi Rizgan <yosriz@gmail.com>
2018-10-02 11:45:55 +0300

    added tests
    * run happy path multiple times to validate consistency
    * verify tamperd json will throw an exception

--- kin-core/src/androidTest/java/kin/core/BackupRestoreTest.java
+++ kin-core/src/androidTest/java/kin/core/BackupRestoreTest.java
@@ -5,6 +5,7 @@ import static org.hamcrest.CoreMatchers.isA;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
 
+import java.util.UUID;
 import kin.core.exception.CryptoException;
 import org.json.JSONException;
 import org.junit.Rule;
@@ -21,10 +22,14 @@ public class BackupRestoreTest {
 
     @Test
     public void backupAndRestore_Success() throws CryptoException {
-        KeyPair keyPair = KeyPair.random();
-        String exportedJson = backupRestore.exportWallet(keyPair, "1234567890abcefghijkl");
-        KeyPair importKeyPair = backupRestore.importWallet(exportedJson, "1234567890abcefghijkl");
-        assertThat(importKeyPair.getSecretSeed(), equalTo(keyPair.getSecretSeed()));
+        for (int i = 0; i < 50; i++) {
+            KeyPair keyPair = KeyPair.random();
+            String passpharse = UUID.randomUUID().toString();
+            String exportedJson = backupRestore.exportWallet(keyPair, passpharse);
+            KeyPair importKeyPair = backupRestore.importWallet(exportedJson, passpharse);
+            assertThat(importKeyPair.getAccountId(), equalTo(keyPair.getAccountId()));
+            assertThat(importKeyPair.getSecretSeed(), equalTo(keyPair.getSecretSeed()));
+        }
     }
 
     @Test
@@ -42,6 +47,34 @@ public class BackupRestoreTest {
         expectedEx.expectCause(isA(JSONException.class));
 
         backupRestore.importWallet("not a real json!!", "123456");
+    }
+
+    @Test
+    public void import_TamperedSaltJson_CryptoException() throws CryptoException {
+        expectedEx.expect(CryptoException.class);
+
+        testImportBackup("{\n"
+                + "  \"pkey\" : \"GAQJH2KSSWOTX3LCEESPIJPY73QRCH55OBCCM3SYZ3EEWSTMJ4NYNT6S\",\n"
+                + "  \"seed\" : \"f5b162bf9bfa93922b709b00a89e5bf7f61eef38717b35dabbabc73a68be77e2b498c5697f99c3f70882a8a11cc5e34f88b6f069f47443dbfa031fadd12e8b6af1cc142c902cfef9\",\n"
+                // change last bit in seed (first ones are salt)
+                + "  \"salt\" : \"d00564d4887b4ccade9f2b63211c37c3\"\n"
+                + "}",
+            "123456",
+            "GAQJH2KSSWOTX3LCEESPIJPY73QRCH55OBCCM3SYZ3EEWSTMJ4NYNT6S");
+    }
+
+    @Test
+    public void import_TamperedSeedJson_CryptoException() throws CryptoException {
+        expectedEx.expect(CryptoException.class);
+
+        testImportBackup("{\n"
+                + "  \"pkey\" : \"GAQJH2KSSWOTX3LCEESPIJPY73QRCH55OBCCM3SYZ3EEWSTMJ4NYNT6S\",\n"
+                // change last bit in seed (first ones are nonce)
+                + "  \"seed\" : \"f5b162bf9bfa93922b709b00a89e5bf7f61eef38717b35dabbabc73a68be77e2b498c5697f99c3f70882a8a11cc5e34f88b6f069f47443dbfa031fadd12e8b6af1cc142c902cfef8\",\n"
+                + "  \"salt\" : \"d00564d4887b4ccade9f2b63211c37c4\"\n"
+                + "}",
+            "123456",
+            "GAQJH2KSSWOTX3LCEESPIJPY73QRCH55OBCCM3SYZ3EEWSTMJ4NYNT6S");
     }
 
     @Test
